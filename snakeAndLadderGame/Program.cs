22611295ee486a8aeca0db202e45424ca8a71eaa
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-21 16:07:24 +0530

    [aditi] Add: Snake and Ladder project files added

---
+++ snakeAndLadderGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace snakeAndLadderGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game!");
+        }
+    }
+}
