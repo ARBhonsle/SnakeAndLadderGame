5123d13305026823d882dcd74dd44523030dbd9a
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-22 23:17:59 +0530

    [aditi] Refactor: Two player plays until one player wins

--- snakeAndLadderGame/Program.cs
+++ snakeAndLadderGame/Program.cs
@@ -8,13 +8,7 @@ namespace snakeAndLadderGame
         {
             Console.WriteLine("Welcome to Snake and Ladder Game!");
             ISnakeAndLadder play = new SnakeAndLadder();
-            //player.ShowPosition();
-            //player.DiceValue();
-            //player.PlayOption();
-            //player.Play();
-            //player.PlayExact();
-            //player.ShowNumberDicePlayed();
-            play.TwoPlayerGame();
+            play.Play();
         }
     }
 }
