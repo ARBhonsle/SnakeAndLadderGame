9fa8ad08559f0e163ff11a10dbb116a03ea2560c
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-22 22:49:45 +0530

    [aditi] Refactor: Player plays until reaches exact win position

--- snakeAndLadderGame/SnakeAndLadder.cs
+++ snakeAndLadderGame/SnakeAndLadder.cs
@@ -12,66 +12,8 @@ namespace snakeAndLadderGame
         {
             this.position = 0;
         }
-        public void ShowPosition()
-        {
-            Console.WriteLine("Player is at position :" + this.position);
-        }
-        public void DiceValue()
-        {
-            Random random = new Random();
-            this.diceNumber = random.Next(0, 6) + 1;
-            Console.WriteLine("Dice Rolled :" + diceNumber);   
-        }
-        public void PlayOption()
-        {
-            Random random = new Random();
-            this.playOption = random.Next(0, 3);
-            switch (playOption)
-            {
-                case 0: Console.WriteLine("No Play");
-                    break;
-                case 1: Console.WriteLine("Ladder");
-                    this.position += this.diceNumber;
-                    break;
-                case 2: Console.WriteLine("Snake");
-                    this.position -= this.diceNumber;
-                    break;
-            }
-        }
+
         public void Play()
-        {
-            while (this.position < this.WIN_POSITION)
-            {
-                Random random = new Random();
-                this.diceNumber = random.Next(0, 6) + 1;
-                this.playOption = random.Next(0, 3);
-                Console.WriteLine("Player is at position :" + this.position);                
-                Console.WriteLine("Dice Rolled :" + diceNumber);
-                switch (playOption)
-                {
-                    case 0:
-                        Console.WriteLine("No Play");
-                        break;
-                    case 1:
-                        Console.WriteLine("Ladder");
-                        this.position += this.diceNumber;                        
-                        break;
-                    case 2:
-                        Console.WriteLine("Snake");
-                        if (this.position > this.diceNumber)
-                        {
-                            this.position -= this.diceNumber;
-                        }
-                        else
-                        {
-                            this.position = 0;
-                        }
-                        break;
-                }
-            }
-            Console.WriteLine("Player Wins");
-        }
-        public void PlayExact()
         {
             while (this.position < this.WIN_POSITION)
             {
@@ -87,7 +29,7 @@ namespace snakeAndLadderGame
                         break;
                     case 1:
                         Console.WriteLine("Ladder");
-                        if ((this.WIN_POSITION - this.position) >= this.diceNumber)
+                        if (this.WIN_POSITION >= (this.position + this.diceNumber))
                         {
                             this.position += this.diceNumber;
                         }
